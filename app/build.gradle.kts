03a19dc62933670f5fc70268b9606d9ecc7d3bbd
DREAMWORLD <fenillathiya.7seasol@gmail.com>
2025-03-21 17:04:46 +0530

    .aab file send version name: 5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gallery.photos.editpic"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
     }
 
     buildTypes {
